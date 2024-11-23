8114ad0c0df4143464313be8ba074ef1a483faf7
BaconButPro <leminhnhat843@gmail.com>
2024-11-23 10:39:50 +0700

    Update Bypass.user.js

--- Install-Userscript/Bypass.user.js
+++ Install-Userscript/Bypass.user.js
@@ -44,12 +44,12 @@
 // @match       *://sub4unlock.com/*
 // @match       *://mendationforc.info/*
 // @match       *://flux.li/android/external/start.php?HWID=*
+// @resource    notifCss https://cdn.jsdelivr.net/gh/f3oall/awesome-notifications/dist/style.css
 // @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
 // @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
-// @require     https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/Main.js
 // @require     https://update.greasyfork.org/scripts/484331/1308737/customnotifications.js
-// @resource    notifCss https://cdn.jsdelivr.net/gh/f3oall/awesome-notifications/dist/style.css
+// @require     https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/Main.js
 // @run-at      document-end
 // @grant       GM_getResourceText
 // @grant       GM_xmlhttpRequest
