f7e0e8d0a8410665324b8e75c3faf87b1a8ab478
BaconButPro <leminhnhat843@gmail.com>
2024-12-09 17:50:15 +0700

    Create BypassViolent.user.js

---
+++ Install-Userscript/BypassViolent.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name        Bypass Key System
+// @homepageURL https://discord.gg/gFNAH7WNZj
+// @namespace   https://tampermonkey.net/
+// @version     2.9.7
+// @description A userscript designed to bypass restrictions and streamline user experiences by automating bypass processes for various stages and challenges
+// @author      Bacon But Pro
+// @match       *://linkvertise.com/*/*
+// @match       *://linkvertise.com/?iwantbypass=*
+// @match       *://socialwolvez.com/*
+// @match       *://rekonise.com/*
+// @match       *://mboost.me/a/*
+// @match       *://loot-link.com/s?*
+// @match       *://loot-links.com/s?*
+// @match       *://lootlink.org/s?*
+// @match       *://lootlinks.co/s?*
+// @match       *://gateway.platoboost.com/a/*
+// @match       *://*.keyrblx.com/*
+// @match       *://lootdest.info/s?*
+// @match       *://lootdest.org/s?*
+// @match       *://mobile.codex.lol/*
+// @match       *://pandadevelopment.net/getkey?*
+// @match       *://lootdest.com/s?*
+// @match       *://links-loot.com/s?*
+// @match       *://linksloot.net/s?*
+// @match       *://spdmteam.com/key-system*
+// @match       *://*/recaptcha/*
+// @match       *://*.hcaptcha.com/*hcaptcha-challenge*
+// @match       *://*.hcaptcha.com/*checkbox*
+// @match       *://*.hcaptcha.com/*captcha*
+// @match       *://bot-hosting.net/panel/*
+// @match       *://trigonevo.com/getkey/?hwid=*
+// @match       *://trigonevo.fun/whitelist/*
+// @match       *://work.ink/*/*
+// @match       *://workink.net/*/*
+// @match       *://paster.so/*
+// @match       *://bstlar.com/*
+// @match       *://sub2unlock.com/*
+// @match       *://*.sub2get.com/*
+// @match       *://boost.ink/*
+// @match       *://social-unlock.com/*
+// @match       *://getkey.relzscript.xyz/*
+// @match       *://dusarisalary.com/*
+// @match       *://bloggingdaze.com/*
+// @match       *://sub4unlock.com/*
+// @match       *://mendationforc.info/*
+// @match       *://flux.li/android/external/start.php?HWID=*
+// @match       *://ads.luarmor.net/*
+// @resource    notifCss https://cdn.jsdelivr.net/gh/f3oall/awesome-notifications/dist/style.css
+// @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
+// @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require     https://update.greasyfork.org/scripts/484331/1308737/customnotifications.js
+// @require     https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/Mainv.js
+// @run-at      document-end
+// @grant       GM_getResourceText
+// @grant       GM_xmlhttpRequest
+// @grant       GM_notification
+// @grant       GM_openInTab
+// @grant       GM_addStyle
+// @license     Bacon
+// @connect     linkvertise.com
+// @connect     short-jambo.com
+// @connect     api.codex.lol
+// @connect     cdn.jsdelivr.net
+// @connect     spdmteam.com
+// @connect     api-gateway.platoboost.com
+// @connect     api.keyrblx.com
+// @connect     keyrblx.com
+// @connect     gateway.platoboost.com
+// @connect     ads.luarmor.net
+// @connect     pandadevelopment.net
+// @connect     hohohubv-ac90f67762c4.herokuapp.com
+// @icon        https://cdn141.picsart.com/351217840073211.png
+// @downloadURL https://github.com/BaconButPro/ron12373/raw/Premium/Install-Userscript/Bypass.user.js
+// @updateURL https://github.com/BaconButPro/ron12373/raw/Premium/Install-Userscript/Bypass.user.js
+// ==/UserScript==
+
+function APIKEY() {
+    const APIKEY = '';
+    return APIKEY;
+}
