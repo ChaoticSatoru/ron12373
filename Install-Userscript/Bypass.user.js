309c6231a1bf440d03c0d904501566e2578840bf
BaconButPro <leminhnhat843@gmail.com>
2024-12-30 20:12:54 +0700

    Update Bypass.user.js

--- Install-Userscript/Bypass.user.js
+++ Install-Userscript/Bypass.user.js
@@ -2,7 +2,7 @@
 // @name        Bypass Key System
 // @homepageURL https://discord.gg/gFNAH7WNZj
 // @namespace   https://tampermonkey.net/
-// @version     3.0.5
+// @version     3.0.6
 // @description A userscript designed to bypass restrictions and streamline user experiences by automating bypass processes for various stages and challenges
 // @author      Bacon But Pro
 // @match       *://linkvertise.com/*/*
@@ -45,6 +45,7 @@
 // @match       *://mendationforc.info/*
 // @match       *://flux.li/android/external/start.php?HWID=*
 // @match       *://flux.li/android/external/check1.php*
+// @match       *://flux.li/android/external/main.php*
 // @match       *://ads.luarmor.net/*
 // @resource    notifCss https://cdn.jsdelivr.net/gh/f3oall/awesome-notifications/dist/style.css
 // @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
@@ -79,4 +80,10 @@
 function APIKEY() {
     const APIKEY = '';
     return APIKEY;
+}
+
+function config() {
+    const fluxus_copy = false;
+    const keyrblx_copy = false;
+    return { fluxus_copy, keyrblx_copy };
 }
