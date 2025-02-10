053cbbc43409b026ef5e4b9444d2be1be3faa0f8
BaconButPro <leminhnhat843@gmail.com>
2025-02-10 11:11:32 +0700

    Delete Bypass-Userscript/API.js

--- Bypass-Userscript/API.js
+++
@@ -1,3 +0,0 @@
-window.BaconButProAPI = "https://baconbutpro.onrender.com/bypass?url=";
-window.BaconButProVersion = "https://baconbutpro.onrender.com/Version";
-window.BaconButProKey = "tBzFLEb08706DP";
