6e07372c5eb6ae861e82c0f3fdb842036a598aa6
BaconButPro <leminhnhat843@gmail.com>
2025-02-10 11:11:21 +0700

    Update API.js

--- Bypass-Userscript/API.js
+++ Bypass-Userscript/API.js
@@ -1,2 +1,3 @@
 window.BaconButProAPI = "https://baconbutpro.onrender.com/bypass?url=";
 window.BaconButProVersion = "https://baconbutpro.onrender.com/Version";
+window.BaconButProKey = "tBzFLEb08706DP";
