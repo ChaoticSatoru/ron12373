01aa8e1d1d9e2760a0bcb80fc21175f6ad217d57
BaconButPro <leminhnhat843@gmail.com>
2025-02-10 11:27:57 +0700

    Create siu.js

---
+++ Bypass-Userscript/siu.js
@@ -0,0 +1,7 @@
+window.BaconButProAPI = "https://baconbutpro.onrender.com/bypass?url=";
+window.BaconButProVersion = "https://check-whitelist.vercel.app/Version";
+window.BaconButProVersion2 = "https://baconbutpro.onrender.com/Version";
+window.BaconButProKey = "tBzFLEb08706DP";
+window.IwoozieAPI = "https://iwoozie.baby/api/free/bypass?url=";
+window.BaconButProUpdates = "https://check-whitelist-six.vercel.app/updates/userscript/";
+window.BaconButProUpdates2 = "https://baconbutpro.onrender.com/updates/userscript/";
