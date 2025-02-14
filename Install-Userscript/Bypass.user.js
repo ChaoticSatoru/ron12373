7cc70c6bb27b50b3830c8fb0e7f666f40a3ceaf0
BaconButPro <leminhnhat843@gmail.com>
2025-02-14 18:51:53 +0700

    Update Bypass.user.js

--- Install-Userscript/Bypass.user.js
+++ Install-Userscript/Bypass.user.js
@@ -6,6 +6,7 @@
 // @description A userscript designed to bypass restrictions and streamline user experiences by automating bypass processes for various stages and challenges
 // @author      Bacon But Pro
 // @match       *://*/
+// @require     https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/Main.js
 // @license     Bacon
 // @icon        https://cdn141.picsart.com/351217840073211.png
 // @downloadURL https://github.com/BaconButPro/ron12373/raw/Premium/Install-Userscript/Bypass.user.js
