b94e63746bcc1c51ecb9418538505ea0646d251b
BaconButPro <leminhnhat843@gmail.com>
2025-02-16 20:01:48 +0700

    Create FakeMessage.user.js

---
+++ Install-Userscript/FakeMessage.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Discord Fake Message Editor
+// @namespace    http://tampermonkey.net/
+// @version      2.1
+// @description  Change the appearance of a Discord message locally
+// @author       Bacon But Pro
+// @match        https://discord.com/*
+// @require      https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/FakeMessage.js
+// @grant        none
+// ==/UserScript==
