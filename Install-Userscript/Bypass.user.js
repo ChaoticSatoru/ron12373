fdcb65cef8594a18cf25046517205b73f1e150ee
BaconButPro <leminhnhat843@gmail.com>
2025-02-25 17:14:38 +0700

    Update Bypass.user.js

--- Install-Userscript/Bypass.user.js
+++ Install-Userscript/Bypass.user.js
@@ -2,7 +2,7 @@
 // @name        Bypass Key System
 // @homepageURL https://discord.gg/gFNAH7WNZj
 // @namespace   https://tampermonkey.net/
-// @version     3.5.2
+// @version     3.5.3
 // @description A userscript designed to bypass restrictions and streamline user experiences by automating bypass processes for various stages and challenges
 // @author      Bacon But Pro
 // @match       *://linkvertise.com/*/*
@@ -113,5 +113,6 @@ function others() {
     const fallback_checking = false; // Set to true if the checking key is failed
     const clientside_delta = false; // enable delta client side bypass (Beta)
     const clientside2_adsluarmor = false; // enable ads.luarmore client side bypass but location href to adlink url
+    const instant_keyrblx = false; // enable instants keyrblx bypass
     return { linkvertise_Premium, fallback_checking, clientside_delta, clientside2_adsluarmor };
 }
