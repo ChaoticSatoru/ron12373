75ed74ec94431fb0069c4165427196d4345d2f99
BaconButPro <leminhnhat843@gmail.com>
2025-02-25 11:18:25 +0700

    Create Test2.js

---
+++ Bypass-Userscript/Test2.js
@@ -0,0 +1,26 @@
+(function() {
+    const _0x1a2b = [
+        atob('aHR0cHM6Ly8uLi4='), // Encoded API URL placeholders
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('QmFjb25CdXRQcm9LZXk='), // Encoded API key placeholder
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('aHR0cHM6Ly8uLi4='),
+        atob('QXRoZXJBUElLZXk=') // Encoded API key placeholder
+    ];
+    window[atob('QmFjb25CdXRQcm9BUEk=')] = _0x1a2b[0];
+    window[atob('QmFjb25CdXRQcm9WZXJzaW9u')] = _0x1a2b[1];
+    window[atob('QmFjb25CdXRQcm9WZXJzaW9uMg==')] = _0x1a2b[2];
+    window[atob('QmFjb25CdXRQcm9LZXk=')] = _0x1a2b[3];
+    window[atob('SXdvb3ppZUFQSSA=')] = _0x1a2b[4];
+    window[atob('QmFjb25CdXRQcm9VcGRhdGVz')] = _0x1a2b[5];
+    window[atob('QmFjb25CdXRQcm9VcGRhdGVzMg==')] = _0x1a2b[6];
+    window[atob('QmFjb25CdXRQcm9Mb2dJUCA=')] = _0x1a2b[7];
+    window[atob('QmFjb25CdXRQcm9Mb2dJUDI=')] = _0x1a2b[8];
+    window[atob('QXRoZXJBUEk=')] = _0x1a2b[9];
+    window[atob('QXRoZXJBUElLZXk=')] = _0x1a2b[10];
+})();
